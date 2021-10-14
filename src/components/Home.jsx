1c9cf5d5797498b54e683d85fb4a852f146d60de
12345120 <hehe9343@gmail.com>
2021-10-14 04:10:21 -0400

    Home, LeftBar Skeleton. TODO list and Notes

---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../css/Home.css";
+import LeftBar from "./LeftBar";
+
+function Home() {
+  return (
+    <div className="home">
+      <LeftBar></LeftBar>
+      
+      <div className="home__header">HEON YIM</div>
+      <div className="home__content">sdfsdfds</div>
+      
+      <div className="test">
+        <div className="test2"></div>
+      </div>
+      <div className="test"></div>
+      
+      <div className="test"></div>
+      
+      <div className="test"></div>
+      
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/LeftBar.jsx
@@ -0,0 +1,11 @@
+import "../css/LeftBar.css";
+
+function LeftBar() {
+  return <div className="leftbar">
+    <div className="leftbar__name">HEON YIM</div>
+    {/* TODO: cool image/mark inbetween name and button to fill gap */}
+    <div className="leftbar__button">TOP</div>
+  </div>;
+}
+
+export default LeftBar;
