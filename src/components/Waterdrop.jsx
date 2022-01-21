0228a57b47ee3bb44050800c01afc5b79152abdd
12345120 <hehe9343@gmail.com>
2022-01-21 01:54:36 -0500

    polish Waterdrop click animation

--- src/components/Waterdrop.jsx
+++ src/components/Waterdrop.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import "../styles/Waterdrop.css";
 
 function Waterdrop() {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <div className="Waterdrop">
-      <div></div>
+      <div
+        className={`Waterdrop_body ${
+          clicked ? "Waterdrop_body__opac" : ""
+        }`}
+      ></div>
+      
+      <div
+        className={`Waterdrop_circleWrapper ${
+          clicked ? "Waterdrop_circleWrapper___bounce" : ""
+        }`}
+      >
+        <div
+          className={`Waterdrop_circle ${
+            clicked ? "Waterdrop_circle___expand" : ""
+          }`}
+          onClick={() => setClicked(true)}
+        ></div>
+      </div>
     </div>
   );
 }
