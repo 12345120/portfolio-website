1bbe58154b66fafdfb1e0d1a6acd30e3d083db87
12345120 <hehe9343@gmail.com>
2022-01-23 04:46:03 -0500

    loading text path animation experiments

--- src/components/Waterdrop.jsx
+++ src/components/Waterdrop.jsx
@@ -1,36 +1,41 @@
-import { useState } from "react";
+import { forwardRef, useEffect, useRef, useState } from "react";
 import "../styles/Waterdrop.css";
 
-function Waterdrop() {
+export default function Waterdrop() {
   const [clicked, setClicked] = useState(false);
   const [hovered, setHovered] = useState(false);
-  
+  const charRef = useRef(null);
+  const pathRef = useRef(null);
+  const [pathLength, setPathLength] = useState(0);
+
+  useEffect(() => {
+    setPathLength(pathRef.current.getTotalLength());
+  }, [pathRef]);
 
   return (
     <div className="Waterdrop">
       <div
-        className={`Waterdrop_body ${
-          clicked ? "Waterdrop_body__opac" : ""
-        }`}
+        className={`Waterdrop_body ${clicked ? "Waterdrop_body__opac" : ""}`}
       ></div>
-      
-      
-      <svg className="Waterdrop_svg" viewBox="0 0 500 500">
-        <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97">
-          <text>
-            <textpath></textpath>
-          </text>
-        </path>
+
+      <h1
+        className={`Waterdrop_text ${
+          hovered ? "Waterdrop_text___unblur" : ""
+        } ${clicked ? "Waterdrop_text___fadeOut" : ""}`}
+      >
+        Click!
+      </h1>
+
+      <svg visibility={"hidden"} style={{ position: "absolute" }}>
+        <path
+          ref={pathRef}
+          d="M 0 0 C 85.5 -25.2 102 50 8 88 C -86 117 -190 -43 -2.1 -86.4 L 193 -88"
+        ></path>
       </svg>
-      
-      <h1 className={`Waterdrop_text ${
-        hovered ? 'Waterdrop_text___unblur' : ''
-      } ${
-        clicked ? 'Waterdrop_text___fadeOut' : ''
-      }`}>Click!</h1>
-      
-      <h1 className="Waterdrop_loadingText">Loading</h1>
-      
+      <h1 className="Waterdrop_loadingText">
+        <TextSplit text={"Loading"} ref={charRef} pathLength={pathLength} />
+      </h1>
+
       <div
         className={`Waterdrop_circleWrapper ${
           clicked ? "Waterdrop_circleWrapper___bounce" : ""
@@ -49,4 +54,40 @@ function Waterdrop() {
   );
 }
 
-export default Waterdrop;
+const TextSplit = forwardRef(({ text, pathLength }, ref) => {
+  const fullText = text;
+  let characters = [];
+  const numChars = fullText.length;
+  for (let i = 0; i < fullText.length; i++) {
+    characters.push(fullText.charAt(i));
+  }
+
+  const [width, setWidth] = useState("");
+
+  useEffect(() => {
+    setWidth(ref.current.offsetWidth);
+  }, [ref]);
+
+  console.log("width: ", width);
+  console.log("path length: ", pathLength);
+
+  return (
+    <span className="word">
+      {characters.map((c, index) => (
+        <span
+          key={index}
+          ref={ref}
+          className="char"
+          style={{
+            "--char-index": index,
+            "--char-width": width + "px",
+            "--num-chars": numChars,
+            "--path-len": pathLength + "px",
+          }}
+        >
+          {c}
+        </span>
+      ))}
+    </span>
+  );
+});
